5c70635427918157a8f8313d3d72eee774d78688
freemanlingli <3436070@qq.com>
2017-10-11 19:18:59 +0800

    本地提交

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 1.10 开发完毕" );
+        System.out.println( "Hello World! 1.12" );
     }
 }
